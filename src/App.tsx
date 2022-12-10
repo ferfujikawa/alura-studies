fa29df009b90b50443af95bfc851bb5c9c167062
ferfujikawa@gmail.com <ferfujikawa@gmail.com>
2022-12-10 10:56:32 -0300

    Criação do componente Lista.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Botao from './components/Botao';
+import Lista from './components/Lista';
 
 function App() {
   return (
     <div className="App">
-          <Botao/>
+          <Botao />
+          <Lista />
     </div>
   );
 }
