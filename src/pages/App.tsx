390bf74b88609c058c7ea6c09646ed8948acf8cc
ferfujikawa@gmail.com <ferfujikawa@gmail.com>
2022-12-11 19:20:23 -0300

    Criação do componente Cronometro.

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Cronometro from '../components/Cronometro';
 import Formulario from '../components/Formulario';
 import Lista from '../components/Lista';
 import style from './App.module.scss';
@@ -6,10 +7,11 @@ import style from './App.module.scss';
 function App() {
     return (
         <div className={style.AppStyle}>
-          <Formulario />
-          <Lista />
-    </div>
-  );
+            <Formulario />
+            <Lista />
+            <Cronometro />
+        </div>
+    );
 }
 
 export default App;
