bf70dce6c725f6567534bb66dba7ebff48bcdf4d
ferfujikawa@gmail.com <ferfujikawa@gmail.com>
2022-12-18 10:56:43 -0300

    Estilização do item quando a tarefa for completada.

--- src/components/Lista/Item/index.tsx
+++ src/components/Lista/Item/index.tsx
@@ -19,7 +19,7 @@ export default function Item(
             className={
                 `${style.item} ${selecionado ? style.itemSelecionado : ''} ${completado ? style.itemCompletado : ''}`
             }
-            onClick={() => selecionaTarefa(
+            onClick={() => !completado && selecionaTarefa(
                 {
                     tarefa,
                     tempo,
@@ -31,6 +31,7 @@ export default function Item(
         >
             <h3> {tarefa} </h3>
             <span> {tempo} </span>
+            {completado && <span className={style.concluido} aria-label="tarefa completada"></span>}
         </li>
     )
 }
